aba4dca84924ae252467f190e93ef09322acad32
JoJoDin <jojov69v@gmail.com>
2016-04-21 15:31:49 -0400

    Start of health Stuff
    Not finished, need to pull changes and see if i can fix issues/bugs

---
+++ Assets/FIRE.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FIRE : MonoBehaviour {
+
+    RectTransform rect;
+    Image image;
+    private const float sizeLiar = 2.700352f;
+
+	// Use this for initialization
+	void Start () {
+        rect = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        FireSize();
+	}
+
+    public void FireSize() {
+
+    }
+}
